fe3620227dcac0944bb9d2a44c590a28fb695e93
rudalson <rudalson@gmail.com>
2023-10-28 21:58:02 +0900

    modify : HomeScreen finished

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import {
+  Dimensions,
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
 import ChatFaceData from '../services/ChatFaceData';
 
 const HomeScreen = () => {
-  const chatFaceData = ChatFaceData;
-  const [selectedChatFaceData, setSelectedChatFaceData] = useState();
+  const [selectedChatFaceData, setSelectedChatFaceData] = useState(
+    ChatFaceData[0]
+  );
 
   useEffect(() => {
-    console.log(chatFaceData[0]);
-    setSelectedChatFaceData(chatFaceData[0]);
+    console.log(ChatFaceData[0]);
   }, []);
+
+  const onChatFacePress = (id) => {
+    setSelectedChatFaceData(ChatFaceData[id]);
+  };
 
   return (
     <View style={{ alignItems: 'center', paddingTop: 90 }}>
@@ -18,12 +30,68 @@ const HomeScreen = () => {
       </Text>
       <Text
         style={[
-          { color: selectedChatFaceData.primary },
+          { color: selectedChatFaceData?.primary },
           { fontSize: 30, fontWeight: 'bold' },
         ]}
       >
         I am {selectedChatFaceData?.name}
       </Text>
+
+      <Image
+        source={{
+          uri: selectedChatFaceData.image,
+        }}
+        style={{ width: 150, height: 150, marginTop: 20 }}
+      />
+      <Text style={{ marginTop: 30, fontSize: 25 }}>How Can I help you?</Text>
+
+      <View
+        style={{
+          marginTop: 20,
+          backgroundColor: '#f5f5f5',
+          alignItems: 'center',
+          height: 110,
+          padding: 10,
+          borderRadius: 10,
+        }}
+      >
+        <FlatList
+          data={ChatFaceData}
+          horizontal
+          renderItem={({ item }) =>
+            selectedChatFaceData.id !== item.id && (
+              <TouchableOpacity
+                style={{ margin: 15 }}
+                onPress={() => {
+                  onChatFacePress(item.id);
+                }}
+              >
+                <Image
+                  source={{ uri: item.image }}
+                  style={{ width: 40, height: 40 }}
+                />
+              </TouchableOpacity>
+            )
+          }
+        />
+        <Text style={{ marginTop: 5, fontSize: 17, color: '#B0B0B0' }}>
+          Choose Your Fav ChatBuddy
+        </Text>
+      </View>
+      <TouchableOpacity
+        style={[
+          { backgroundColor: selectedChatFaceData.primary },
+          {
+            padding: 17,
+            width: Dimensions.get('screen').width * 0.6,
+            borderRadius: 100,
+            alignItems: 'center',
+            marginTop: 30,
+          },
+        ]}
+      >
+        <Text style={{ fontSize: 16, color: '#fff' }}>Let's Chat</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/services/ChatFaceData.js
+++ src/services/ChatFaceData.js
@@ -1,36 +1,40 @@
 export default ChatFaceData = [
   {
-    id: 1,
+    id: 0,
     name: 'Noyi',
-    image: 'https://res.cloudinary.com/dknvsbuyy/image/upload',
+    image:
+      'https://miro.medium.com/v2/resize:fit:1400/1*vkeAZdTYbZ2Lal07DZKIqw.png',
     primary: '#FFC107',
     secondary: '',
   },
   {
-    id: 2,
+    id: 1,
     name: 'Pogu',
-    image: 'https://res.cloudinary.com/dknvsbuyy/image/upload',
+    image:
+      'https://i0.wp.com/grupovip.com/wp-content/uploads/2019/11/chat-bot-for-social-networking.jpg?fit=1000%2C1000&ssl=1',
     primary: '#E53057',
     secondary: '',
   },
   {
-    id: 3,
+    id: 2,
     name: 'Nista',
-    image: 'https://res.cloudinary.com/dknvsbuyy/image/upload',
+    image:
+      'https://magnitudemanagement.com/wp-content/uploads/2019/01/bot-icon-2883144-1024x1024.png',
     primary: '#3B96D2',
     secondary: '',
   },
   {
-    id: 4,
+    id: 3,
     name: 'Estor',
-    image: 'https://res.cloudinary.com/dknvsbuyy/image/upload',
+    image:
+      'https://png.pngtree.com/png-vector/20220611/ourmid/pngtree-smiling-cute-robot-chat-bot-logo-png-image_5008711.png',
     primary: '#37474F',
     secondary: '',
   },
   {
-    id: 5,
+    id: 4,
     name: 'Pega',
-    image: 'https://res.cloudinary.com/dknvsbuyy/image/upload',
+    image: 'https://static.thenounproject.com/png/1156281-200.png',
     primary: '#2473FE',
     secondary: '',
   },
